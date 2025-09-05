e4d38ec57d0676979dcfb2705904ab4f6c49c251
Het72 <hetshah1072@gmail.com>
2025-09-05 22:53:47 +0530

    port added

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const config = require("./config/config");
 require("dotenv").config();
 
 const app = express();
-const PORT = config.PORT;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
